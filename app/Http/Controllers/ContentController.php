a87a31a2c1c7a9fd92e1c6ef36cf0633c8154eb5
rachelxx03 <53119641+rachelxx03@users.noreply.github.com>
2023-05-02 13:09:01 -0300

    delete

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -97,35 +97,7 @@ class ContentController extends Controller
         ]);        //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $content = Content::findOrFail($id);
